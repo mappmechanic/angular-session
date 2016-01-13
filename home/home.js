f2afa1d581f8c0a241387d8d66b2f52a27f9c49a
Rahat Khanna <rahat.khanna@flipkart.com>
2016-01-13 08:11:17 +0530

    Ng Expressions Examples

--- home/home.js
+++ home/home.js
@@ -30,4 +30,18 @@ angular.module('myApp.home', ['ngRoute'])
 
 .controller('SubHomeCtrl', ['$scope',function($scope) {
 	$scope.welcomeMessage = "Sub Home Level Welcome";
+
+    $scope.cost = '500';
+    $scope.quantity = 0;
+
+
+    $scope.user = {
+        firstname: "Rahat",
+        lastname: "Khanna",
+        age:25
+    }
+
+    $scope.products = ['product1','product2','product3','product4'];
+    $scope.selectedProductIndex = 1;
+
 }]);
