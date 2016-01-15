8a673e1109cbcf7cad90bd74743e3e22082c7612
Rahat Khanna <rahat.khanna@flipkart.com>
2016-01-15 08:15:13 +0530

    Exercise 6 Completed

--- forms/forms.js
+++ forms/forms.js
@@ -43,8 +43,23 @@ angular.module('myApp.forms', ['ngRoute'])
       }
 
       $scope.reset();
-}])
+}]);
 
+angular.module('myApp')
+.filter('reverse', function() {
+  return function(input, uppercase) {
+    input = input || '';
+    var out = "";
+    for (var i = 0; i < input.length; i++) {
+      out = input.charAt(i) + out;
+    }
+    // conditional based on optional argument
+    if (uppercase) {
+      out = out.toUpperCase();
+    }
+    return out;
+  };
+})
 
 var app = angular.module('myApp');
 
