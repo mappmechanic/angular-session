16b8f121ad2dbb619039d51b6a67c7f11655e53d
Rahat Khanna <rahat.khanna@flipkart.com>
2016-01-16 06:10:00 +0530

    Exercise 1 Completed

--- forms/forms.js
+++ forms/forms.js
@@ -3,117 +3,90 @@
 angular.module('myApp.forms', ['ngRoute'])
 
 .config(['$routeProvider', function($routeProvider) {
-  $routeProvider.when('/forms', {
+  $routeProvider.when('/forms/:model/:id?', {
     templateUrl: 'forms/forms.html',
     controller: 'FormsController'
   });
 }])
 
-.controller('FormsController', ['$scope', function($scope) {
-      $scope.master = {};
+.controller('FormsController', ['$scope','$routeParams', function($scope,$routeParams) {
+  $scope.model = $routeParams['model'] ? $routeParams['model'] : '' ;
+ 
+  $scope.id = $routeParams['id'] ? $routeParams['id'] : 0;
+ 
+  $scope.action = $routeParams['action'] ? $routeParams['action'] : 'add';
 
-      $scope.names = [
-        {name:'Jani',location:'Norway'},
-        {name:'Hege',location:'Sweden'},
-        {name:'Kai',location:'Denmark'},
-        {name:'User1',location:'Delhi'},
-        {name:'User2',location:'North America'},
-        {name:'User3',location:'South America'}
-    ];
-
-
-      $scope.update = function(user) {
-        if($scope.form.$valid) {
-          $scope.master = angular.copy(user);
-        }
-      };
-
-      $scope.reset = function(form) {
-        if (form) {
-          form.$setPristine();
-          form.$setUntouched();
-        }
-        $scope.user = angular.copy({});
-        $scope.master = angular.copy({});
-      };
-
-
-      $scope.typeSelected = function() {
-        alert('New Type Selected');
-      }
-
-      $scope.reset();
 }]);
 
-angular.module('myApp')
-.filter('reverse', function() {
-  return function(input, uppercase) {
-    input = input || '';
-    var out = "";
-    for (var i = 0; i < input.length; i++) {
-      out = input.charAt(i) + out;
-    }
-    // conditional based on optional argument
-    if (uppercase) {
-      out = out.toUpperCase();
-    }
-    return out;
-  };
-})
+// angular.module('myApp')
+// .filter('reverse', function() {
+//   return function(input, uppercase) {
+//     input = input || '';
+//     var out = "";
+//     for (var i = 0; i < input.length; i++) {
+//       out = input.charAt(i) + out;
+//     }
+//     // conditional based on optional argument
+//     if (uppercase) {
+//       out = out.toUpperCase();
+//     }
+//     return out;
+//   };
+// })
 
-var app = angular.module('myApp');
+// var app = angular.module('myApp');
 
-var INTEGER_REGEXP = /^\-?\d+$/;
-app.directive('integer', function() {
-  return {
-    require: 'ngModel',
-    link: function(scope, elm, attrs, ctrl) {
-      ctrl.$validators.integer = function(modelValue, viewValue) {
-        if (ctrl.$isEmpty(modelValue)) {
-          // consider empty models to be valid
-          return true;
-        }
+// var INTEGER_REGEXP = /^\-?\d+$/;
+// app.directive('integer', function() {
+//   return {
+//     require: 'ngModel',
+//     link: function(scope, elm, attrs, ctrl) {
+//       ctrl.$validators.integer = function(modelValue, viewValue) {
+//         if (ctrl.$isEmpty(modelValue)) {
+//           // consider empty models to be valid
+//           return true;
+//         }
 
-        if (INTEGER_REGEXP.test(viewValue)) {
-          // it is valid
-          return true;
-        }
+//         if (INTEGER_REGEXP.test(viewValue)) {
+//           // it is valid
+//           return true;
+//         }
 
-        // it is invalid
-        return false;
-      };
-    }
-  };
-});
+//         // it is invalid
+//         return false;
+//       };
+//     }
+//   };
+// });
 
-app.directive('username', function($q,$timeout) {
-  return {
-    require: 'ngModel',
-    link: function(scope, elm, attrs, ctrl) {
-    var usernames = ['Jim', 'John', 'Jill', 'Jackie'];
+// app.directive('username', function($q,$timeout) {
+//   return {
+//     require: 'ngModel',
+//     link: function(scope, elm, attrs, ctrl) {
+//     var usernames = ['Jim', 'John', 'Jill', 'Jackie'];
 
-      ctrl.$asyncValidators.username = function(modelValue, viewValue) {
+//       ctrl.$asyncValidators.username = function(modelValue, viewValue) {
 
-        if (ctrl.$isEmpty(modelValue)) {
-          // consider empty model valid
-          return $q.when();
-        }
+//         if (ctrl.$isEmpty(modelValue)) {
+//           // consider empty model valid
+//           return $q.when();
+//         }
 
-        var def = $q.defer();
-        usernames = usernames.map(function(username){ return username.toLowerCase(); });
-        $timeout(function() {
-          // Mock a delayed response
-          if (usernames.indexOf(modelValue) === -1) {
-            // The username is available
-            def.resolve();
-          } else {
-            def.reject();
-          }
+//         var def = $q.defer();
+//         usernames = usernames.map(function(username){ return username.toLowerCase(); });
+//         $timeout(function() {
+//           // Mock a delayed response
+//           if (usernames.indexOf(modelValue) === -1) {
+//             // The username is available
+//             def.resolve();
+//           } else {
+//             def.reject();
+//           }
 
-        }, 2000);
+//         }, 2000);
 
-        return def.promise;
-      };
-    }
-  };
-});
+//         return def.promise;
+//       };
+//     }
+//   };
+// });
